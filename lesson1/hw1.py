56fb70e80170b58a9102b27e20a81fed2ad4fc21
serj-goa <bsy.prog@gmail.com>
2020-12-11 09:12:19 +0200

    Create hw1.py
    The code returning the entered variables

---
+++ lesson1/hw1.py
@@ -0,0 +1,6 @@
+name = input("Введите имя: ")
+surname = input("Введите Фамилию: ")
+age = input("Укажите ваш возраст: ")
+hi_string = f"Hello {surname} {name}, твой возраст: {age}"
+
+print(hi_string)
